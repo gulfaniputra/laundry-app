7446a8c4770c4c4f38861fd0364a0d19de19c605
Gulfani Putra <gulfaniputra@gmail.com>
2023-11-30 11:25:01 +0800

    Generate HTML from hardcoded laundry order data

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,18 +1,35 @@
-const laundryOrder = {
-  orderOne: {
-    order: "1",
-    id: "621ACY",
-    weight: "2",
-    price: "3500",
-    status: "Ready to pickup",
-    pickupBy: "Tomorrow",
-  },
-  orderTwo: {
-    order: "2",
-    id: "836BWF",
-    weight: "7",
-    price: "50000",
-    status: "Washing in progress",
-    pickupBy: "pending",
-  },
+const orderOneData = {
+  order: "1",
+  id: "621ACY",
+  weight: "2",
+  price: "35000",
+  status: "Ready to pickup",
+  pickupBy: "Tomorrow",
+};
+
+const orderTwoData = {
+  order: "2",
+  id: "836BWF",
+  weight: "7",
+  price: "50000",
+  status: "Washing in progress",
+  pickupBy: "Pending",
+};
+
+document.getElementById("button").onclick = function () {
+  document.getElementById("orderOneOrder").textContent = orderOneData.order;
+  document.getElementById("orderOneId").textContent = orderOneData.id;
+  document.getElementById("orderOneWeight").textContent = orderOneData.weight;
+  document.getElementById("orderOnePrice").textContent = orderOneData.price;
+  document.getElementById("orderOneStatus").textContent = orderOneData.status;
+  document.getElementById("orderOnePickupBy").textContent =
+    orderOneData.pickupBy;
+  document.getElementById("orderTwoOrder").textContent = orderTwoData.order;
+  document.getElementById("orderTwoOrder").textContent = orderTwoData.order;
+  document.getElementById("orderTwoId").textContent = orderTwoData.id;
+  document.getElementById("orderTwoWeight").textContent = orderTwoData.weight;
+  document.getElementById("orderTwoPrice").textContent = orderTwoData.price;
+  document.getElementById("orderTwoStatus").textContent = orderTwoData.status;
+  document.getElementById("orderTwoPickupBy").textContent =
+    orderTwoData.pickupBy;
 };
