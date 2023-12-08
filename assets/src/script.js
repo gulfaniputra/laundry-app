2b946e77188f5403a69bb9cc5aaa0c4372cc7647
Gulfani Putra <gulfaniputra@gmail.com>
2023-12-08 10:35:42 +0800

    Refactor JS with nested arrays & 'forEach' loop

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,35 +1,58 @@
-const orderOneData = {
-  order: "1",
-  id: "621ACY",
-  weight: "2",
-  price: "35000",
-  status: "Ready to pickup",
-  pickupBy: "Tomorrow",
-};
+const laundryData = [
+  {
+    order: "1",
+    id: "621ACY",
+    weight: "2",
+    price: "35000",
+    status: "Ready to pickup",
+    pickupBy: "Tomorrow",
+  },
+  {
+    order: "2",
+    id: "836BWF",
+    weight: "7",
+    price: "50000",
+    status: "Washing in progress",
+    pickupBy: "Pending",
+  },
+];
 
-const orderTwoData = {
-  order: "2",
-  id: "836BWF",
-  weight: "7",
-  price: "50000",
-  status: "Washing in progress",
-  pickupBy: "Pending",
-};
+function simulateLoginState() {
+  laundryData.forEach(function (data) {
+    let laundryOrder = document.createElement("h3");
+    laundryOrder.innerHTML = "Order: " + data.order;
+    document.getElementById("contents").append(laundryOrder);
 
-document.getElementById("button").onclick = function () {
-  document.getElementById("orderOneOrder").textContent = orderOneData.order;
-  document.getElementById("orderOneId").textContent = orderOneData.id;
-  document.getElementById("orderOneWeight").textContent = orderOneData.weight;
-  document.getElementById("orderOnePrice").textContent = orderOneData.price;
-  document.getElementById("orderOneStatus").textContent = orderOneData.status;
-  document.getElementById("orderOnePickupBy").textContent =
-    orderOneData.pickupBy;
-  document.getElementById("orderTwoOrder").textContent = orderTwoData.order;
-  document.getElementById("orderTwoOrder").textContent = orderTwoData.order;
-  document.getElementById("orderTwoId").textContent = orderTwoData.id;
-  document.getElementById("orderTwoWeight").textContent = orderTwoData.weight;
-  document.getElementById("orderTwoPrice").textContent = orderTwoData.price;
-  document.getElementById("orderTwoStatus").textContent = orderTwoData.status;
-  document.getElementById("orderTwoPickupBy").textContent =
-    orderTwoData.pickupBy;
-};
+    laundryData.forEach(function (data) {
+      let laundryOrder = document.createElement("p");
+      laundryOrder.innerHTML = "ID: " + data.id;
+      document.getElementById("contents").append(laundryOrder);
+    });
+
+    laundryData.forEach(function (data) {
+      let laundryOrder = document.createElement("p");
+      laundryOrder.innerHTML = "Weight: " + data.weight;
+      document.getElementById("contents").append(laundryOrder);
+    });
+
+    laundryData.forEach(function (data) {
+      let laundryOrder = document.createElement("p");
+      laundryOrder.innerHTML = "Price: " + data.price;
+      document.getElementById("contents").append(laundryOrder);
+    });
+
+    laundryData.forEach(function (data) {
+      let laundryOrder = document.createElement("p");
+      laundryOrder.innerHTML = "Status: " + data.status;
+      document.getElementById("contents").append(laundryOrder);
+    });
+
+    laundryData.forEach(function (data) {
+      let laundryOrder = document.createElement("p");
+      laundryOrder.innerHTML = "Pickup by: " + data.pickupBy;
+      document.getElementById("contents").append(laundryOrder);
+    });
+  });
+}
+
+simulateLoginState();
